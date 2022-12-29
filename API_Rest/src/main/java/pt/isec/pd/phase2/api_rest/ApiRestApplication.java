cc3aed0386b388cc3b6511f3277f940d213780e5
Hugo Ferreira <84676077+hooper1337@users.noreply.github.com>
2022-12-29 19:21:44 +0000

    More developing(requests arent being recognized)

--- API_Rest/src/main/java/pt/isec/pd/phase2/api_rest/ApiRestApplication.java
+++ API_Rest/src/main/java/pt/isec/pd/phase2/api_rest/ApiRestApplication.java
@@ -2,13 +2,16 @@ package pt.isec.pd.phase2.api_rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class ApiRestApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ApiRestApplication.class, args);
 	}
 
--- API_Rest/src/main/java/pt/isec/pd/phase2/api_rest/controller/ShowController.java
+++ API_Rest/src/main/java/pt/isec/pd/phase2/api_rest/controller/ShowController.java
@@ -1,4 +1,27 @@
 package pt.isec.pd.phase2.api_rest.controller;
 
-public class ShowController {
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import pt.isec.pd.phase2.api_rest.model.Show;
+import pt.isec.pd.phase2.api_rest.service.ShowService;
+
+import java.util.List;
+
+@RestController
+public class ShowController
+{
+    private final ShowService showService;
+
+    @Autowired
+    public ShowController(ShowService showService) {
+        this.showService = showService;
+    }
+
+    @GetMapping("/show")
+    public ResponseEntity<List<Show>> getFilteredShows(@RequestParam(value = "date", required = true) String date)
+    {
+        return ResponseEntity.ok().body(showService.getShowsByDate(date));
+    }
 }
